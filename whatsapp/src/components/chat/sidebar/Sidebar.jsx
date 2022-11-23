f5582bd7e93b55e53c1cc7dd50208767eeb0c8a6
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-11-23 17:10:53 +0530

    search bar ui added

--- whatsapp/src/components/chat/sidebar/Sidebar.jsx
+++ whatsapp/src/components/chat/sidebar/Sidebar.jsx
@@ -1,10 +1,14 @@
-import { Box } from '@mui/material'
+import { Box ,styled} from '@mui/material'
 import React from 'react'
-import { Header } from './Header'
+import { Header } from './Header';
+import { Search } from './Search';
+
+
 export const Sidebar = () => {
   return (
     <Box>
         <Header/>
+        <Search/>
     </Box>
   )
 }
